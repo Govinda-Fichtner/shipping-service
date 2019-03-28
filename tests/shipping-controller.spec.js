86f7afa9f384beb4332c168e487c0d9fa3f03d16
Govinda Fichtner <govinda.fichtner@googlemail.com>
2019-03-28 16:01:23 +0100

    fix tests by remove obsolete code

--- tests/shipping-controller.spec.js
+++ tests/shipping-controller.spec.js
@@ -1,15 +1,3 @@
-var ShippingController = require('../src/controllers/shipping-controller')
-
-describe('Shipping controller', function () {
-  var shippingCtrl = new ShippingController()
-
-  it('Should calculate correct shipping ', async function () {
-    let shipping = await shippingCtrl.getItemShipping({ id: 1, type: 'standard' })
-    expect(shipping).toBe(0.5)
-  })
-
-})
-
 /* tests/shipping-controller.test.js */
 var sinon = require('sinon')
 var ShippingController = require('../src/controllers/shipping-controller')
